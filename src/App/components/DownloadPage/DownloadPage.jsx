9ecb03fc32984016a9ec681e3276f6d7a0ca5705
Mirzoyoqub <rashidovmirzoyoqub81@gmail.com>
2022-08-19 18:10:36 +0500

    completed #103

---
+++ src/App/components/DownloadPage/DownloadPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./DownloadPage.css";
+
+const DownloadPage = () => {
+  return (
+    <section className="download">
+      <div className="left_d">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg"
+          alt=""
+        />
+      </div>
+      <div className="right_d">
+        <h1>Download your shows to watch offline.</h1>
+        <h2>Save your favorites easily and always have something to watch.</h2>
+      </div>
+    </section>
+  );
+};
+
+export default DownloadPage;
--- src/App/pages/Home/Home.jsx
+++ src/App/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import DownloadPage from "../../components/DownloadPage/DownloadPage";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <DownloadPage />
     </div>
   );
 };
