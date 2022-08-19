8e9039c1b07ef9966df116f5a13981f58b61f1a9
KABIR <kabir_karamatjanov@mail.ru>
2022-08-19 18:50:40 +0500

    smart project finished

--- src/App/pages/Home/Home.jsx
+++ src/App/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Smart from "../../components/Smart/Smart";
+
+
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Smart/>
     </div>
   );
 };
