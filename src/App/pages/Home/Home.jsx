3bea3c4a73503078eeb5649d56f84bf8232cbbf7
KABIR <kabir_karamatjanov@mail.ru>
2022-08-20 12:52:38 +0500

    footer finished

--- src/App/pages/Home/Home.jsx
+++ src/App/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Smart from "../../components/Smart/Smart";
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <Smart/>
+      <Footer/>
     </div>
   );
 };
