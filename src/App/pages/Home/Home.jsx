2ee46fa4c6aedbdcfc32f012e1d16d75d54664e3
Mirzoyoqub <rashidovmirzoyoqub81@gmail.com>
2022-08-20 13:06:59 +0500

    completed 104

--- src/App/pages/Home/Home.jsx
+++ src/App/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DownloadPage from "../../components/DownloadPage/DownloadPage";
 import Header from "../../components/Header/Header";
+import Stream from "../../components/Stream/Stream";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <DownloadPage />
+      <Stream />
     </div>
   );
 };
